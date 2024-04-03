843e9f32179e29370b4d0862b305e7b6d5a0a182
khushal <khushal2004a@gmail.com>
2024-04-03 10:58:15 +0530

    zod and purchanse logic

---
+++ zod/src/index.ts
@@ -0,0 +1,23 @@
+import z from 'zod';
+
+export const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional(),
+});
+
+export type SignupType = z.infer<typeof signupSchema>;
+
+export const signinSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+export type SigninType = z.infer<typeof signinSchema>;
+
+export const createCourseSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+})
+
+export type CreateCourseType = z.infer<typeof createCourseSchema>
